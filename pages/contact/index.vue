729014a1f8bf46c00f8c676bc7ce50b2a97a0444
Pj Ofodile <pjfides@gmail.com>
2018-12-17 19:58:52 +0100

    added  names to form

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -3,9 +3,9 @@
         <h3>Get in touch with Asa.</h3> <br><br><br>
 
         <form action="../success/" name="contact" method="POST" data-netlify="true">
-            <input type="text" placeholder="name"> <br><br>
-            <input type="email" placeholder="your email"><br><br>
-            <input type="text" placeholder="your message"> <br><br>
+            <input type="text" placeholder="name" name="Name"> <br><br>
+            <input type="email" placeholder="your email" name="Email address"><br><br>
+            <input type="text" placeholder="your message" name="Message"> <br><br>
             <p>Hey don't spam me!</p>
             <a href="../success/">sucess</a>
             <button>Submit</button>
